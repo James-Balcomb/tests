00495c1eba326eabc10799270fa9f811990184c1
James Balcomb <jamesbalcomb@yahoo.com>
2022-02-20 15:11:18 -0700

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,1 +1,8 @@
 // JavaScript source code
+function printEvenNumbers() {
+    for (var i = 0; i <= 10; i++) {
+        if (i % 2 == 0) {
+            document.getElementById("EvenNumbers").innerText += (i + "\n");
+        }
+    }
+}
